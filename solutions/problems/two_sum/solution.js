c6f5917a07ed6f44b865c2b63e035b09458c0cd9
Andrew Cooke <105688564+andcooke@users.noreply.github.com>
2022-01-20 16:02:22 +0000

    Sync LeetCode submission - Two Sum (javascript)

---
+++ solutions/problems/two_sum/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+    var res = []
+    
+    for (i = 0; i < nums.length; i++){
+        for (j = i+1; j < nums.length; j++){
+            sum = nums[i]
+            sum += nums[j]
+                if (sum === target){
+                    res.push(i, j)
+                }
+        }
+    }
+    return res
+};
